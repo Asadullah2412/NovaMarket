5e8be021b3f7e01a1489a1f2bcb92181cb35fa52
Asadullah2412 <asadullahmohammad2412@gmail.com>
2026-08-19 13:26:28 +0300

    v0.1 kong gateway

--- service/product-service/database/database.py
+++ service/product-service/database/database.py
@@ -1,8 +1,13 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'postgresql://postgres:asad123@localhost:5432/products'
+URL_DATABASE = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:asad123@localhost:5432/products"
+)
 
 # creating engine 
 engine = create_engine(URL_DATABASE)
