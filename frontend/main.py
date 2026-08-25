cb2358569132f3553b3e6e3f6ac6b576280a4961
Asadullah2412 <asadullahmohammad2412@gmail.com>
2026-08-25 12:45:55 +0300

    ui

--- frontend/main.py
+++ frontend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -8,7 +9,13 @@ templates = Jinja2Templates(directory="templates")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def home(request: Request):
     return templates.TemplateResponse(
